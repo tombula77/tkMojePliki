af16a4010dfa95ae0439bc6478d15d0211840a87
tombula77 <tombula77@gmail.com>
2022-04-20 13:59:24 +0200

    Rewrited PyQT5 -> Tk

--- Main.py
+++ Main.py
@@ -6,6 +6,8 @@ from configparser import ConfigParser
 
 import ConfigCreate
 
+# Read app configuration file or create new file when not present
+
 parser = ConfigParser()
 cfgfile = "config.ini"
 
@@ -21,64 +23,25 @@ height = parser.get('main', 'height')
 winparam = str(width+'x'+height+'+'+x_init+'+'+y_init)
 
 
-def quit():
-    print("quitting...")
-    sys.exit(0)
+def savepos():
+    # Save current window position and size
 
+    global parser
+    global cfgfile
 
-def savepos(self):
-    act_width = str(self.width())
-    act_height = str(self.height())
-    act_x_init = str(self.pos().x())
-    act_y_init = str(self.pos().y())
+    geometry = GUI.geometry()
+    act_width = GUI.winfo_width()
+    act_height = GUI.winfo_height()
+    temp, act_x_init, act_y_init = geometry.split("+")
     parser.read(cfgfile)
-    parser.set('main', 'width', act_width)
-    parser.set('main', 'height', act_height)
-    parser.set('main', 'x_init', act_x_init)
-    parser.set('main', 'y_init', act_y_init)
+    parser.set('main', 'width', str(act_width))
+    parser.set('main', 'height', str(act_height))
+    parser.set('main', 'x_init', str(act_x_init))
+    parser.set('main', 'y_init', str(act_y_init))
     file = open(cfgfile, 'w')
     parser.write(file, space_around_delimiters=False)
     file.close()
 
-def initui():
-    # Definiowanie akcji paska menu
-    exitAction = QAction(QIcon('images/exit.png'), '&Wyjście', self)
-    exitAction.setShortcut('Ctrl+Q')
-    exitAction.setStatusTip('Zamknij program')
-    exitAction.triggered.connect(qApp.quit)
-
-    savePosition = QAction(QIcon('images/diskette2.png'), 'Zapisz ustawienia okna', self)
-    savePosition.setStatusTip('Zapisuje rozmiar oraz pozycję okna na pulpicie')
-    savePosition.triggered.connect(self.savepos)
-
-    # Konstruowanie paska menu
-    menubar = self.menuBar()
-    filemenu = menubar.addMenu('Pli&k')
-    filemenu.addAction(exitAction)
-    commenu = menubar.addMenu('Po&lecenia')
-    viewmenu = menubar.addMenu('Wid&ok')
-    cfgmenu = menubar.addMenu('&Ustawienia')
-    cfgmenu.addAction(savePosition)
-
-    # Konstrukcja paska narzędziowego
-    tb1 = self.addToolBar("Pasek 1")
-    new = QAction(QIcon("images/newfile.png"), "Nowy plik", self)
-    tb1.setIconSize(QtCore.QSize(32, 32))
-    tb1.setMovable(False)
-    tb1.addAction(new)
-    self.addToolBarBreak()
-
-    tb2 = self.addToolBar("Pasek 2")
-    tb2.setIconSize(QtCore.QSize(16, 16))
-    tb2.setMovable(False)
-    tb2.addAction(new)
-
-    self.setGeometry(x_init, y_init, width, height)
-    self.setWindowTitle('Moje Pliki')
-    self.setStyleSheet("background-color: lightgray;")
-    self.setWindowIcon(QtGui.QIcon('images/logo.png'))
-    self.statusBar()
-    self.show()
 
 if __name__ == "__main__":
     GUI = Tk()
@@ -86,10 +49,25 @@ if __name__ == "__main__":
     GUI.title('Moje Pliki')
     GUI.configure(bg='dark gray')
 
+    # Create app menu
     menubar = Menu(GUI)
     filemenu = Menu(menubar, tearoff=0)
     menubar.add_cascade(label="Pliki", underline=0, menu=filemenu)
     filemenu.add_command(label="Wyjście", underline=1, command=quit)
+    commenu = Menu(menubar, tearoff=0)
+    menubar.add_cascade(label="Polecenia", underline=0, menu=commenu)
+    commenu.add_command(label="Wyjście", underline=1, command=quit)
+    viewmenu = Menu(menubar, tearoff=0)
+    menubar.add_cascade(label="Widok", underline=0, menu=viewmenu)
+    viewmenu.add_command(label="Wyjście", underline=1, command=quit)
+    cfgmenu = Menu(menubar, tearoff=0)
+    menubar.add_cascade(label="Konfiguracja", underline=0, menu=cfgmenu)
+    cfgmenu.add_command(label="Zapisz ustawienia", underline=1, command=savepos)
     GUI.config(menu=menubar)
+
+    # Status bar
+
+#    statusbar = Label(GUI, text="on the way…", bd=1, relief=SUNKEN, anchor=W)
+#    statusbar.pack(side=BOTTOM, fill=X)
 
     GUI.mainloop()
